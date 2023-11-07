54c334302c68c3dcdd2667414d9f00c82fa236d8
Kiran <smkiran.09@gmail.com>
2023-11-08 00:54:41 +0530

    Updated concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,53 @@
-import React from "react";
-import Header from "./Components/Header";
-import TabComponents from "./Components/TabComponents";
+import { useState } from "react";
 
-const App = () => {
-  function handleSelect() {
-    alert("clicked");
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  const [selectedTopic, setSelectedTopic] = useState("Components");
+
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    setSelectedTopic(selectedButton);
+    // console.log(selectedTopic);
   }
+
+  console.log("APP COMPONENT EXECUTING");
+
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {selectedTopic}
+        </section>
       </main>
-      <TabComponents onSelect={handleSelect}>TabComponents </TabComponents>
-      <TabComponents>JSX </TabComponents>
-      <TabComponents>Props </TabComponents>
-      <TabComponents>State </TabComponents>
     </div>
   );
-};
+}
 
 export default App;
